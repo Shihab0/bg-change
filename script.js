2952cc866898c8f5d11e70e79784f751e7faed8b
Shihab0 <ataharshihab1@gmail.com>
2023-11-06 22:25:10 +0600

    copy btn added

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ function main() {
   const root = document.getElementById("root");
   const btn = document.getElementById("change-btn");
   const colorCode = document.getElementById("colorCode");
+  const hexCode = document.getElementById("hexCode");
+  const copyBtn = document.getElementById("copy-btn");
 
   btn.addEventListener("click", function () {
     const bgColor = generateRgb();
     root.style.backgroundColor = bgColor;
     colorCode.innerText = bgColor;
+
+    const hexColor = generateHex();
+    hexCode.value = hexColor;
+  });
+
+  copyBtn.addEventListener("click", function () {
+    window.navigator.clipboard.writeText(hexCode.value);
   });
 }
 
@@ -22,4 +31,11 @@ function generateRgb() {
   const blue = Math.floor(Math.random() * 255);
 
   return `rgba(${red}, ${green}, ${blue})`;
+}
+function generateHex() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
